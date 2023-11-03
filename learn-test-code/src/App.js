8c2dd84df1c496a4259bd966e044e2c76ec98578
moonseonyeong <seon_moon@naver.com>
2023-11-03 23:28:30 +0900

    [test] on/off 버튼 클릭 시 style 변경

--- learn-test-code/src/App.js
+++ learn-test-code/src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div className="App">
@@ -12,10 +13,21 @@ function App() {
         <h3 data-testid="counter">{counter}</h3>
         <button data-testid="minus-button"
           onClick={() => setCounter((count) => count - 1)}
-        >-</button>
+          disabled={disabled}
+        >
+          -
+        </button>
         <button data-testid="plus-button"
           onClick={() => setCounter((count) => count + 1)}
-        >+</button>
+          disabled={disabled}
+        >
+          +
+        </button>
+
+        <button data-testid="on/off-button"
+          style={{ backgroundColor: "blue" }}
+          onClick={() => setDisabled((prev) => !prev)}
+        >on/off</button>
       </header>
     </div>
   );
